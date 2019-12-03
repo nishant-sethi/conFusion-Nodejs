c442f76bc715722a56d8762b21b27bdb77540e6d
nishantsethi <nsethi610@gmail.com>
2019-12-04 01:05:28 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -19,11 +19,36 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
   console.log('Connected to the server');
-},(err) => {
+}, (err) => {
   console.log(err);
 });
 
 var app = express();
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error('Not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+    return;
+  }
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+    next();
+  }
+  else {
+    var err = new Error('Not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+  }
+}
+app.use(auth);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,25 +60,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use('/dishes',dishRouter);
-app.use('/dishes/:dishId',dishRouter);
+app.use('/dishes', dishRouter);
+app.use('/dishes/:dishId', dishRouter);
 
-app.use('/promotions',promoRouter);
-app.use('/promotions/:promoId',promoRouter);
+app.use('/promotions', promoRouter);
+app.use('/promotions/:promoId', promoRouter);
 
-app.use('/leaders',leaderRouter);
-app.use('/leaders/:promoId',leaderRouter);
+app.use('/leaders', leaderRouter);
+app.use('/leaders/:promoId', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
