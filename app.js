746777204c74ec844eef8a02e39c51c9018f0940
nishantsethi <nsethi610@gmail.com>
2019-12-04 01:14:44 +0530

    Cookies

--- app.js
+++ app.js
@@ -25,27 +25,41 @@ connect.then((db) => {
 
 var app = express();
 
+app.use(cookieParser('12345-67890-09876-54321'));
+
 function auth(req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error('Not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-    return;
-  }
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user === 'admin' && pass === 'password') {
-    next();
-  }
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error('Not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user === 'admin' && pass === 'password') {
+      res.cookie('user', 'admin', { signed: true });
+      next();
+    }
+    else {
+      var err = new Error('Not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+    }
+  } 
   else {
-    var err = new Error('Not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
+    if (req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      var err = new Error('Not authenticated!');
+      err.status = 401;
+      next(err);
+    }
   }
 }
 app.use(auth);
