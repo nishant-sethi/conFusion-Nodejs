1e7fabf179186a7bb27c8c41b8cf20f9c5749efb
nishantsethi <nsethi610@gmail.com>
2019-12-24 23:14:12 +0530

    File Upload

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
@@ -60,6 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 
 // catch 404 and forward to error handler
