35cf77a25bc464a06d937be2ece3403f687fee75
Nishant sethi <nsethi610@gmail.com>
2021-04-06 20:19:27 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -23,8 +23,3 @@ User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
 
-/* 
-export const phoneNumberwithCode = (message) => (value) => {    var value_split = value && value.split(';');    var valid = true;    if (value === '') return '';
-    value &&        value_split.map((value) => {            var digits_value = value.trim().replace(/[^0-9]/g, '').length;            if (                digits_value < 5 ||                digits_value > 15 ||                value.endsWith('-') ||                value.startsWith('-') ||                value.includes('--') ||                !/^\+?[0-9-]*(\([0-9]+\)[0-9-]*)*[0-9;-]*$/.test(value.toString())            ) {                valid = false;            }        });    return !valid ? message : '';};-- 
-RegardsNishant Sethi
-*/
